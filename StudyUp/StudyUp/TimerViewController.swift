cd0db7fddc33fdfc2e25fbe1cd4a224dcb37dee1
MitchelEppich <ltory@sc-3100-24.labs.surrey.sfu.ca>
2017-03-05 13:17:37 -0800

    added timer guy

--- StudyUp/StudyUp/TimerViewController.swift
+++ StudyUp/StudyUp/TimerViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class TimerViewController: UIViewController {
+    
+    let courses = ["MSE 483", "CMPT 276", "CMPT 361", "CMPT 363"]
 
+    @IBOutlet weak var timePicker: UIDatePicker!
+    
+    @IBOutlet weak var coursePickerButton: UIButton!
+    
+    @IBOutlet weak var coursePicker: UIPickerView!
+    
+    @IBOutlet weak var smartStudyToggle: UISwitch!
+    
+    @IBOutlet weak var smartStudyView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +33,24 @@ class TimerViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func courseButtonPressed(_ sender: AnyObject) {
+    }
+    
+    @IBAction func smatStudyToggled(_ sender: AnyObject) {
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView)-> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return courses.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent: Int) -> String? {
+        return courses[row]
+    }
+    
     /*
     // MARK: - Navigation
 
