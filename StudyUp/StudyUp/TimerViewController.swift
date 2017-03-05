c5d5887bcb5d26675e01210d282e13143baaee18
Owen <kwoko828@gmail.com>
2017-03-05 11:57:23 -0800

    test shit

--- StudyUp/StudyUp/TimerViewController.swift
+++ StudyUp/StudyUp/TimerViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class TimerViewController: UIViewController {
-
+    
+    var timer = Timer()
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        countingLabel.text = String(counter)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func updateCounter(){
+        countingLabel.text = String(describing: counter+=1)
+    }
+    
+    @IBOutlet var countingLabel: UILabel!
+    
+    @IBAction func startButton(_ sender: Any) {
+        timer = Timer.scheduledTimer(timeInterval: 1, target:self, selector: #selector(TimerViewController.updateCounter), userInfo: nil, repeats: true)
+
+    }
+    
+    @IBAction func breakButton(_ sender: Any) {
+        timer.invalidate()
+    }
+    
+    
+    
+    
+    
     
 
     /*
