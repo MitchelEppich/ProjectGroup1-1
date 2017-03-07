b45cf78b375cb5c1a93f2403f01968d426a94c3c
kityt <jxluo@asb9700-f02.csil.sfu.ca>
2017-03-07 11:49:52 -0800

    Improved comments
    Comment on Profile section code is improved

--- StudyUp/StudyUp/ProfileViewController.swift
+++ StudyUp/StudyUp/ProfileViewController.swift
@@ -8,125 +8,120 @@
 
 import UIKit
 
-    class ProfileViewController: UIViewController,UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+class ProfileViewController: UIViewController,UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    
+    var editEnable = false
+    
+    // cannot be edited
+    @IBOutlet weak var profileName: UILabel!
+    
+    // can be edited
+    @IBOutlet weak var profileBio: UITextView!
+    @IBOutlet weak var profileFaculty: UITextView!
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    
+    // used for connections with the timer
+    @IBOutlet weak var courseName: UILabel!
+    @IBOutlet var courseProgress: UIView!
+    
+    
+    // click for edit
+    @IBOutlet weak var profileEdit: UIButton!
+    @IBOutlet weak var profileDone: UIButton!
+    
+    
+    // hidden
+    @IBOutlet weak var profileImageEdit: UIButton!
+    
+    
+    
+    // ------------UI items listed---------------
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         
-        var editEnable = false
+        //later:  take user data from server/local
         
-        // cannot be edited
- 
-        @IBOutlet weak var profileName: UILabel!
-        
-        
-        
-        // can be edited
-        @IBOutlet weak var profileBio: UITextView!
-        @IBOutlet weak var profileFaculty: UITextView!
-        @IBOutlet weak var profileImage: UIImageView!
-
-        
-        // be in a class?
-        @IBOutlet weak var courseName: UILabel!
-        @IBOutlet var courseProgress: UIView!
+        // set initial status to not editable
+        profileDone.isHidden = true
+        profileBio.isEditable = false
+        profileFaculty.isEditable = false
+        profileImageEdit.isHidden = true
         
         
-        // click to edit
-        @IBOutlet weak var profileEdit: UIButton!
-        @IBOutlet weak var profileDone: UIButton!
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    // click on edit to enable editing on image, bio, and faculty
+    @IBAction func editProfile(_ sender: AnyObject) {
         
-        
-        // hidden
-        @IBOutlet weak var profileImageEdit: UIButton!
-        
-        
-        
-        // ------------UI items listed----------
-        
-        
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            // Do any additional setup after loading the view, typically from a nib.
-            
-            // take user data from server/local
-            
-            profileDone.isHidden = true
+        // if already one edit mode, do not enable again
+        if editEnable {
             profileBio.isEditable = false
             profileFaculty.isEditable = false
             profileImageEdit.isHidden = true
+            editEnable = false
             
-            
+        } else {
+            // allows user to:
+            // edit bio
+            profileBio.isEditable = true
+            // edit faculty name
+            profileFaculty.isEditable = true
+            // edit profile picture
+            profileImageEdit.isHidden = false
+            editEnable = true
         }
         
-        override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-            // Dispose of any resources that can be recreated.
-        }
+    }
+    
+    // when user clicked on done after editing
+    @IBAction func editDone(_ sender: AnyObject) {
         
+        // disables editing
+        profileDone.isHidden = true
+        profileBio.isEditable = false
+        profileFaculty.isEditable = false
+        profileImageEdit.isHidden = true
         
+        profileEdit.isHidden = false
+    }
+    
+    
+    
+    // call image picker to edit profile picture
+    @IBAction func editImage(_ sender: AnyObject) {
         
+        // call out image picker to allow user to select a piture from photolibrary to use for profile picture
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        self.present(picker,animated:true,completion: nil)
         
-        // click edit to enable editing on image, bio, faculty and color
-        @IBAction func editProfile(_ sender: AnyObject) {
-            
-            if editEnable {
-                //profileDone.isHidden = true
-                profileBio.isEditable = false
-                profileFaculty.isEditable = false
-                profileImageEdit.isHidden = true
-                editEnable = false
-                
-                //profileEdit.isHidden = false
-            } else {
-                //profileEdit.isHidden = true
-                //profileDone.isHidden = false
-                
-                // edit bio
-                profileBio.isEditable = true
-                
-                // edit faculty name
-                profileFaculty.isEditable = true
-                
-                
-                // edit profile picture
-                profileImageEdit.isHidden = false
-                editEnable = true
-            }
-            
-            
-        }
+    }
+    // set selected image as profile picture
+    func imagePickerController(_ picker: UIImagePickerController!, didFinishPickingMediaWithInfo info: [String : Any]){
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
         
-        @IBAction func editDone(_ sender: AnyObject) {
-            profileDone.isHidden = true
-            profileBio.isEditable = false
-            profileFaculty.isEditable = false
-            profileImageEdit.isHidden = true
-            
-            profileEdit.isHidden = false
-        }
-        
-        
-        
-        // edit profile picture
-        @IBAction func editImage(_ sender: AnyObject) {
-        
-            let picker = UIImagePickerController()
-            picker.delegate = self
-            picker.allowsEditing = true
-            picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
-            self.present(picker,animated:true,completion: nil)
-            
-        }
-        
-        func imagePickerController(_ picker: UIImagePickerController!, didFinishPickingMediaWithInfo info: [String : Any]){
-            let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-            
-            profileImage.image = selectedImage
-            // Dismiss the picker.
-            dismiss(animated: true, completion: nil)
-        }
-        // finished with the code portion of edit profile picture
-
-        
+        profileImage.image = selectedImage
+        // Dismiss the picker.
+        dismiss(animated: true, completion: nil)
+    }
+    // finished with editing profile picture
+    
+    
 }
 
 
