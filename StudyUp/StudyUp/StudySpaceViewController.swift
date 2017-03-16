fb020cd8154dd3d3a855201eaa1580e4abff0967
ltory <ltory@sc-2300-46.labs.surrey.sfu.ca>
2017-03-16 14:45:20 -0700

    different approach for study spaces, still working

---
+++ StudyUp/StudyUp/StudySpaceViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StudySpaceViewController.swift
+//  StudyUp
+//
+//  Created by Leone Tory on 3/16/17.
+//  Copyright © 2017 SFU Health++. All rights reserved.
+//
+
+import UIKit
+
+class StudySpaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
