df525561d0c8927e3dd449178bb7acb8cdac5c95
ltory <ltory@sc-2300-46.labs.surrey.sfu.ca>
2017-03-16 13:57:13 -0700

    support work in progress

---
+++ StudyUp/StudyUp/SupportViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SupportViewController.swift
+//  StudyUp
+//
+//  Created by Leone Tory on 3/16/17.
+//  Copyright © 2017 SFU Health++. All rights reserved.
+//
+
+import UIKit
+
+class SupportViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func HealthCounsellingPressed(_ sender: AnyObject) {
+        let url = URL(string :"http://www.sfu.ca/students/health/")
+        UIApplication.shared.open(url!)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
